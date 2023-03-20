953dfc3fa6f60ce16a603e422ea150d32467ee45
Esteban Enriquez <juan.esteban.94@outlook.com>
2023-03-20 14:10:54 -0500

    some fixes on setup mysql dev

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -6,3 +6,6 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grant privileges to the user on hbnb_dev_db
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on performance_schema to the user
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
