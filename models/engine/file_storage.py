fafa4ce7c7eedef840984aa91e7db22de8401738
Esteban Enriquez <juan.esteban.94@outlook.com>
2023-03-20 21:10:12 -0500

    some fixes on file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,15 +10,14 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        if cls:
+        if cls is None:
             return self.__objects
         else:
             new_dict = {}
             for key, value in self.__objects.items():
-                if cls.__name__ == value.__class__.__name__:
-                    print("dict")
+                if value.__class__.__name__ == cls.__name__:
                     new_dict[key] = value
-                    return new_dict
+            return new_dict
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -26,12 +25,11 @@ class FileStorage:
 
     def save(self):
         """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
-                temp[key] = val.to_dict()
-            json.dump(temp, f)
+        objects_dict = {}
+        for key, value in self.__objects.items():
+            objects_dict[key] = value.to_dict()
+        with open(self.__file_path, "w", encoding='utf-8') as f:
+            json.dump(objects_dict, f)
 
     def reload(self):
         """Loads storage dictionary from file"""
@@ -47,7 +45,7 @@ class FileStorage:
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
                     'State': State, 'City': City, 'Amenity': Amenity,
                     'Review': Review
-                  }
+                    }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
@@ -59,7 +57,7 @@ class FileStorage:
 
     def delete(self, obj=None):
         """Delete obj from __objects if it's inside, if obj is equal to None, do nothing"""
-        if obj:
+        if obj is not None:
             key = "{}.{}".format(type(obj).__name__, obj.id)
             if key in self.__objects:
                 del self.__objects[key]
