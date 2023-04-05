cff264173dac6cb91c587a2105fcff059983331f
Esteban Enriquez <juan.esteban.94@outlook.com>
2023-04-05 17:55:28 -0500

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
+""" Script that starts a Flask web application """
 from flask import Flask
 
-
 app = Flask(__name__)
-"""decorate"""
+
+
 @app.route('/', strict_slashes=False)
 def hello():
-    """return"""
-    return "Hello HBNB!"
+    """ Function that returns a string """
+    return 'Hello HBNB!'
 
 
-if __name__ == "__main__":
-    """run flask"""
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
