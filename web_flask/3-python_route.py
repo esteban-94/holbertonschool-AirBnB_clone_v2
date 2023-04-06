38b55476804214da1ea54de494ec06a914d09342
Esteban Enriquez <juan.esteban.94@outlook.com>
2023-04-05 21:12:07 -0500

    task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,31 +1,34 @@
 #!/usr/bin/python3
+""" Script that starts a Flask web application """
 from flask import Flask
 
-
 app = Flask(__name__)
-"""decorate"""
+
+
 @app.route('/', strict_slashes=False)
 def hello():
-    """return"""
-    return "Hello HBNB!"
+    """ Function that returns a string """
+    return 'Hello HBNB!'
+
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return "HBNB"
-"""decorate"""
+    """ Function that returns a string """
+    return 'HBNB'
+
+
 @app.route('/c/<text>', strict_slashes=False)
-def c_is_fun(text):
-    """return"""
-    text = text.replace("_", " ")
-    return f"C {text}"
-"""decorate"""
-@app.route("/python/", defaults={"text": "is_cool"})
-@app.route("/python/<text>", strict_slashes=False)
-def python(text):
-    """return"""
-    text = text.replace('_', ' ')
-    return "Python {}".format(text)
+def c(text):
+    """ Function that returns a string """
+    return f"C {text.replace('_', ' ')}"
 
 
-if __name__ == "__main__":
-    """run flask"""
-    app.run(host="0.0.0.0", port=5000)
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    """ Function that returns a string """
+    return f"Python {text.replace('_', ' ')}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
