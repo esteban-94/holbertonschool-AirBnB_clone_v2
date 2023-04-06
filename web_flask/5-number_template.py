ec0abf65ed2cff958c505da42644d7b26f73154c
Esteban Enriquez <juan.esteban.94@outlook.com>
2023-04-05 22:38:23 -0500

    task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,41 +1,45 @@
 #!/usr/bin/python3
+""" Script that starts a Flask web application """
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
-"""decorate"""
+
+
 @app.route('/', strict_slashes=False)
 def hello():
-    """return"""
-    return "Hello HBNB!"
+    """ Function that returns a string """
+    return 'Hello HBNB!'
+
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return "HBNB"
-"""decorate"""
+    """ Function that returns a string """
+    return 'HBNB'
+
+
 @app.route('/c/<text>', strict_slashes=False)
-def c_is_fun(text):
-    """return"""
-    text = text.replace("_", " ")
-    return f"C {text}"
-"""decorate"""
-@app.route("/python/", defaults={"text": "is_cool"})
-@app.route("/python/<text>", strict_slashes=False)
-def python(text):
-    """return"""
-    text = text.replace('_', ' ')
-    return "Python {}".format(text)
-"""decorate"""
-@app.route("/number/<int:n>", strict_slashes=False)
+def c(text):
+    """ Function that returns a string """
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    """ Function that returns a string """
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    """return"""
+    """ Function that returns a string """
     return f"{n} is a number"
-"""decorate"""
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def number_html(n):
-    """return"""
-    return render_template("5-number.html", n=n)
 
 
-if __name__ == "__main__":
-    """run flask"""
-    app.run(host="0.0.0.0", port=5000)
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """ Function that returns a string """
+    return render_template('5-number.html', num=n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
