3d8ccd360194186fa3c723766e585b74f9a59df5
Esteban Enriquez <juan.esteban.94@outlook.com>
2023-04-05 22:45:48 -0500

    task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -39,7 +39,7 @@ def number(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """ Function that returns a string """
-    return render_template('5-number.html', num=n)
+    return render_template('5-number.html', n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
